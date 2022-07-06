532088438499dde452d08d91cd647693f3cf613b
illusionjew <illusion.jew@gmail.com>
2022-07-06 15:57:07 +0700

    3 files modified

--- index.js
+++ index.js
@@ -3,15 +3,32 @@ import './style.css';
 // Import LIFF
 import liff from '@line/liff';
 
+const month_dict = {
+  1: 'มกราคม',
+  2: 'กุมภาพันธ์',
+  3: 'มีนาคม',
+  4: 'เมษายน',
+  5: 'พฤษภาคม',
+  6: 'มิถุนายน',
+  7: 'กรกฎาคม',
+  8: 'สิงหาคม',
+  9: 'กันยายน',
+  10: 'ตุลาคม',
+  11: 'พฤศจิกายน',
+  12: 'ธันวาคม',
+};
 // get html element
 // const html_body = document.getElementById('Body');
 // const profile_url = document.getElementById('UserProfilePictureURL');
 // const display_name = document.getElementById('DisplayName');
 // const status_msg = document.getElementById('LineStatus');
 // const user_email = document.getElementById('UserEmail');
-const prefix_div = document.getElementsByClassName('prefixname-input');
+const birth_date_selector = document.getElementById('BirthDateSelector');
+const birth_month_selector = document.getElementById('BirthMonthSelector');
+const birth_year_selector = document.getElementById('BirthYearSelector');
 
 async function main() {
+  initialBirthSelector();
   // getUserProfile();
   await liff.init({
     liffId: '1657263880-bNJ3z7yx',
@@ -28,3 +45,27 @@ main();
 //   status_msg.innerHTML =
 //     '<b>Line status : ' + user_profile.statusMessage + '</b>';
 // }
+
+function initialBirthSelector() {
+  const current = new Date();
+  for (let i = current.getFullYear(); i > 1930; i--) {
+    var option = document.createElement('option');
+    option.text = i + 543;
+    option.value = i;
+    birth_year_selector.appendChild(option);
+  }
+
+  for (let j = 1; j < 13; j++) {
+    var option = document.createElement('option');
+    option.text = month_dict[j];
+    option.value = j;
+    birth_month_selector.appendChild(option);
+  }
+
+  for (let k = 1; k < 32; k++) {
+    var option = document.createElement('option');
+    option.text = k;
+    option.value = k;
+    birth_date_selector.appendChild(option);
+  }
+}
