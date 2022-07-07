ed70d207e63e1919538abf92cf05134d32e2f1e3
illusionjew <illusion.jew@gmail.com>
2022-07-07 18:01:06 +0700

    2 files modified

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // Import stylesheets
 import './style.css';
+
 // Import LIFF
 import liff from '@line/liff';
+
+import boundary_data from './assets/province.json';
+
+const province_array = boundary_data.Province;
+const district_array = boundary_data.District;
+const subdistrict_array = boundary_data.Subdistrict;
 
 const month_dict = {
   1: 'มกราคม',
@@ -17,22 +24,22 @@ const month_dict = {
   11: 'พฤศจิกายน',
   12: 'ธันวาคม',
 };
+
 // get html element
-// const html_body = document.getElementById('Body');
-// const profile_url = document.getElementById('UserProfilePictureURL');
-// const display_name = document.getElementById('DisplayName');
-// const status_msg = document.getElementById('LineStatus');
-// const user_email = document.getElementById('UserEmail');
 const birth_date_selector = document.getElementById('BirthDateSelector');
 const birth_month_selector = document.getElementById('BirthMonthSelector');
 const birth_year_selector = document.getElementById('BirthYearSelector');
+const province_selector = document.getElementById('ProvinceSelector');
+const district_selector = document.getElementById('DistrictSelector');
+const subdistrict_selector = document.getElementById('SubdistrictSelector');
 
 async function main() {
   initialBirthSelector();
+  initialBoundarySelector();
   // getUserProfile();
   await liff.init({
     liffId: '1657263880-bNJ3z7yx',
-    // withLoginOnExternalBrowser: true,
+    // withLoginOnExternalBrowser: true
   });
 }
 main();
@@ -71,4 +78,22 @@ function initialBirthSelector() {
     option.id = 'date' + k;
     birth_date_selector.appendChild(option);
   }
+}
+
+function initialBoundarySelector() {
+  let province_sorted = province_array.sort((a, b) =>
+    a.name_th > b.name_th ? 1 : b.name_th > a.name_th ? -1 : 0
+  );
+  province_sorted.forEach(item);
+  province_sorted.forEach((province) => {
+    addOptBoundaryToSelector(province_selector, province);
+  });
+}
+
+function addOptBoundaryToSelector(selector, data) {
+  let opt = document.createElement('option');
+  opt.text = data.name_th;
+  opt.value = data.name_th;
+  opt.id = data.name_th;
+  selector.appendChild(opt);
 }
